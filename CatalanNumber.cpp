7a0364f0d97678b3eeb769cd401fbd5d5783e817
Raj Agrahari <81769395+RajAgrahari@users.noreply.github.com>
2022-09-21 10:20:15 +0530

    DAY-4
    MEDIUM

---
+++ CatalanNumber.cpp
@@ -0,0 +1,17 @@
+// https://practice.geeksforgeeks.org/problems/nth-catalan-number0817/1
+cpp_int helper(vector<cpp_int>& dp,int n)
+    {
+        if(n<=1)
+        return 1;
+        if(dp[n]!=-1)
+        return dp[n];
+        cpp_int res = 0;
+        for(int i=0;i<n;i++)
+        res += helper(dp,i)*helper(dp,n-i-1);
+        return dp[n]=res;
+    }
+    cpp_int findCatalan(int n) 
+    {
+        vector<cpp_int> dp(n+1,-1);
+        return helper(dp,n);
+    }
